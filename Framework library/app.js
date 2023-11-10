13e3d76b611a944eebf77f8285784e4720a5fb91
Jose Ivan Campos Chavez <ivan820plus@gmail.com>
2023-11-09 22:52:51 -0600

    added functionality to the mini library

--- Framework library/app.js
+++ Framework library/app.js
@@ -1,2 +1,11 @@
 var g = G$('Ivan', 'Campos', 'es');
+
 g.greet().setLang('en').greet(true).log();
+
+$('#login').click(function () {
+  var loginGrtr = G$('Ivan', 'Campos');
+
+  $('#logindiv').hide();
+
+  loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
